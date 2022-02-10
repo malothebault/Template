693c73e33588b43fa75457c3dbd43fa53bbbba03
Malo <malothebault@lilo.org>
2022-02-10 14:43:15 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,8 +7,8 @@ NEW_PROJECT="USER INPUT"
 read -p "Enter your username (in lower case): " NEW_USERNAME
 read -p "Enter your project name (in lower case): " NEW_PROJECT
 
-find . -type f -exec sed -i "s/$OLD_PROJECT/$NEW_PROJECT/g" {} +
-find . -type f -exec sed -i "s/${OLD_PROJECT^}/${NEW_PROJECT^}/g" {} +
+find . -type f ! -regex '\(.*\/\.git.*\|.*\/assets.*\|.*\/build.*\)' -exec sed -i "s/$OLD_PROJECT/$NEW_PROJECT/g" {} +
+find . -type f ! -regex '\(.*\/\.git.*\|.*\/assets.*\|.*\/build.*\)' -exec sed -i "s/${OLD_PROJECT^}/${NEW_PROJECT^}/g" {} +
 find . -name "com.github.$OLD_USERNAME.$OLD_PROJECT*" -exec rename.ul "$OLD_USERNAME.$OLD_PROJECT" "$NEW_USERNAME.$NEW_PROJECT" '{}' \;
 
 echo "Done! You can start working"
